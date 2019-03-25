b755701d2c894a1f11775118eb9e0c9992e33e0d
farmasek <vojtabaranek@gmail.com>
2019-03-25 16:56:50 +0100

    Updates

---
+++ precache-manifest.125883dc05fe6090f121133d43302733.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c83a14eb166e2f2d42d",
+    "url": "/bsc-fe-test/static/js/runtime~main.4c83a14e.js"
+  },
+  {
+    "revision": "46bfd39876d5a04fb2d9",
+    "url": "/bsc-fe-test/static/js/main.46bfd398.chunk.js"
+  },
+  {
+    "revision": "5928ed4a1040d47d2d66",
+    "url": "/bsc-fe-test/static/js/2.5928ed4a.chunk.js"
+  },
+  {
+    "revision": "46bfd39876d5a04fb2d9",
+    "url": "/bsc-fe-test/static/css/main.0ddf8764.chunk.css"
+  },
+  {
+    "revision": "0ef8f7b537012b96bceabb10990263b6",
+    "url": "/bsc-fe-test/index.html"
+  }
+];
